f242a799003170cdff91f19381d2088ecd6cd0f0
Qrag11 <m.przystawski8@gmail.com>
2026-01-12 18:19:17 +0100

    First commit

---
+++ src/main/java/pl/przystawski/ems/employee_mood_system/EmployeeMoodSystemApplication.java
@@ -0,0 +1,13 @@
+package pl.przystawski.ems.employee_mood_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeMoodSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeMoodSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/przystawski/ems/employee_mood_system/EmployeeMoodSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.przystawski.ems.employee_mood_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeMoodSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
