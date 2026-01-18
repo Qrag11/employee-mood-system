ecc987272ac922c8df6403468b43b00b8a7be5c8
Qrag11 <m.przystawski8@gmail.com>
2026-01-18 16:09:13 +0100

    Add employee entity initialization and update security config

--- src/main/java/pl/przystawski/ems/employee_mood_system/config/security/CustomUserDetailsService.java
+++ src/main/java/pl/przystawski/ems/employee_mood_system/config/security/CustomUserDetailsService.java
@@ -17,7 +17,6 @@ public class CustomUserDetailsService implements UserDetailsService {
 
     @Override
     public UserDetails loadUserByUsername(String username){
-        System.out.println("🔐 Loading user: " + username);
         Employee employee = employeeRepository.findByEmail(username).orElseThrow(() ->
                 new UsernameNotFoundException("employee not find"));
         return new CustomUserDetails(employee);
